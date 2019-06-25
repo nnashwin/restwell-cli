3e4edb58b883ccbd1ad58cb670a7bc3321e1e529
ru-lai <ru.lai.development@gmail.com>
2019-06-24 20:44:29 -0700

    finishes up the main api and creates the server when a valid json file exists at the particular path

--- cli.go
+++ cli.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	pf "github.com/ru-lai/pathfinder"
+	rw "github.com/ru-lai/restwell"
 	"github.com/urfave/cli"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
+
+var addr = "127.0.0.1:12345"
 
 func StartCli(args []string) ([]string, error) {
 	var resp []string
@@ -30,6 +38,21 @@ func StartCli(args []string) ([]string, error) {
 			Aliases: []string{"crs"},
 			Usage:   "Creates a rest server from a json file stored on disk",
 			Action: func(c *cli.Context) error {
+				if pf.DoesExist(c.Args().First()) == false {
+					err = errors.New(fmt.Sprintf("The file path \"%s\" does not exist. Enter a valid path and try again", c.Args().First()))
+					return err
+				}
+
+				jStr, err := ioutil.ReadFile(c.Args().First())
+				if err != nil {
+					return err
+				}
+
+				mux := rw.CreateMuxFromJSON(string(jStr))
+				s := http.Server{Handler: mux, Addr: addr}
+				fmt.Printf("Now listening on %s...\n", addr)
+				log.Fatal(s.ListenAndServe())
+
 				return nil
 			},
 		},
